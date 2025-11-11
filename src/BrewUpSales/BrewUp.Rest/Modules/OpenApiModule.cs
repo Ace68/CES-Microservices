61d45b6aaf73e3edfa8a5ffff1cadd8ce9fafee7
Alberto Acerbis <alberto.acerbis@gmail.com>
2025-11-11 19:39:10 +0100

    Upgrade to .NET10

--- src/BrewUpSales/BrewUp.Rest/Modules/OpenApiModule.cs
+++ src/BrewUpSales/BrewUp.Rest/Modules/OpenApiModule.cs
@@ -1,47 +1,47 @@
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Scalar.AspNetCore;
 
 namespace BrewUp.Rest.Modules;
 
 public class OpenApiModule : IModule
 {
-    public bool IsEnabled => true;
-    public int Order => 0;
+  public bool IsEnabled => true;
+  public int Order => 0;
 
-    public IServiceCollection Register(WebApplicationBuilder builder)
+  public IServiceCollection Register(WebApplicationBuilder builder)
+  {
+    builder.Services.AddOpenApi(options =>
     {
-        builder.Services.AddOpenApi(options =>
-        {
-            options.AddDocumentTransformer((document, _, _) =>
+      options.AddDocumentTransformer((document, _, _) =>
+          {
+            document.Servers = [new OpenApiServer { Url = "/" }];
+            document.Info = new OpenApiInfo
             {
-                document.Servers = [new OpenApiServer {Url = "/"}];
-                document.Info = new OpenApiInfo
-                {
-                    Title = "BrewUp Sales API",
-                    Version = "v1.0",
-                    Description = "BrewUp API for managing a Brewery sales system.",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "BrewUp Sales Team"
-                    }
-                };
+              Title = "BrewUp Sales API",
+              Version = "v1.0",
+              Description = "BrewUp API for managing a Brewery Sales system.",
+              Contact = new OpenApiContact
+              {
+                Name = "BrewUp Sales Team"
+              }
+            };
 
-                return Task.CompletedTask;
-            });
-        });
+            return Task.CompletedTask;
+          });
+    });
 
-        return builder.Services;
-    }
+    return builder.Services;
+  }
 
-    public WebApplication Configure(WebApplication app)
+  public WebApplication Configure(WebApplication app)
+  {
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
     {
-        app.MapOpenApi();
-        app.MapScalarApiReference(options =>
-        {
-            options.WithTitle("BrewUp API")
-                .WithTheme(ScalarTheme.None);
-        });
+      options.WithTitle("BrewUp API")
+              .WithTheme(ScalarTheme.None);
+    });
 
-        return app;
-    }
+    return app;
+  }
 }
--- src/BrewUpWarehouse/BrewUp.Rest/Modules/OpenApiModule.cs
+++ src/BrewUpWarehouse/BrewUp.Rest/Modules/OpenApiModule.cs
@@ -1,47 +1,47 @@
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Scalar.AspNetCore;
 
 namespace BrewUp.Rest.Modules;
 
 public class OpenApiModule : IModule
 {
-    public bool IsEnabled => true;
-    public int Order => 0;
+  public bool IsEnabled => true;
+  public int Order => 0;
 
-    public IServiceCollection Register(WebApplicationBuilder builder)
+  public IServiceCollection Register(WebApplicationBuilder builder)
+  {
+    builder.Services.AddOpenApi(options =>
     {
-        builder.Services.AddOpenApi(options =>
-        {
-            options.AddDocumentTransformer((document, _, _) =>
+      options.AddDocumentTransformer((document, _, _) =>
+          {
+            document.Servers = [new OpenApiServer { Url = "/" }];
+            document.Info = new OpenApiInfo
             {
-                document.Servers = [new OpenApiServer {Url = "/"}];
-                document.Info = new OpenApiInfo
-                {
-                    Title = "BrewUp Warehouse API",
-                    Version = "v1.0",
-                    Description = "BrewUp API for managing the a Brewery warehouse.",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "BrewUp-Warehouse Team",
-                    }
-                };
+              Title = "BrewUp Warehouse API",
+              Version = "v1.0",
+              Description = "BrewUp API for managing the a Brewery Warehouse.",
+              Contact = new OpenApiContact
+              {
+                Name = "BrewUp-Warehouse Team",
+              }
+            };
 
-                return Task.CompletedTask;
-            });
-        });
+            return Task.CompletedTask;
+          });
+    });
 
-        return builder.Services;
-    }
+    return builder.Services;
+  }
 
-    public WebApplication Configure(WebApplication app)
+  public WebApplication Configure(WebApplication app)
+  {
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
     {
-        app.MapOpenApi();
-        app.MapScalarApiReference(options =>
-        {
-            options.WithTitle("BrewUp API")
-                .WithTheme(ScalarTheme.None);
-        });
+      options.WithTitle("BrewUp API")
+              .WithTheme(ScalarTheme.None);
+    });
 
-        return app;
-    }
+    return app;
+  }
 }
